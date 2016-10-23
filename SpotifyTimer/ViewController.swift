bc0d2204db7efcb637cf2c7e7e9f9536e8cf0834
shoya <12c1055@gmail.com>
2016-10-23 16:35:45 +0900

    Add ControlHepler

--- SpotifyTimer/ViewController.swift
+++ SpotifyTimer/ViewController.swift
@@ -13,21 +13,6 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Stop
-        if let path = Bundle.main.path(forResource: "StopSpotify", ofType:"scpt") {
-            let task = Process()
-            task.launchPath = "/usr/bin/osascript"
-            task.arguments = [path]
-            task.launch()
-        }
-        
-        // Play
-        if let path = Bundle.main.path(forResource: "PlaySpotify", ofType:"scpt") {
-            let task = Process()
-            task.launchPath = "/usr/bin/osascript"
-            task.arguments = [path]
-            task.launch()
-        }
     }
 
     override var representedObject: Any? {
