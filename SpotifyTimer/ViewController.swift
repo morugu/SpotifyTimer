da8b649ffea44df1085e216c63ca36123afca322
shoya <12c1055@gmail.com>
2016-10-24 21:54:57 +0900

    Fix count down logic

--- SpotifyTimer/ViewController.swift
+++ SpotifyTimer/ViewController.swift
@@ -10,15 +10,59 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var timerPicker: NSDatePicker!
+    @IBOutlet weak var timerLabel: NSTextField!
+    @IBOutlet weak var startButton: NSButton!
+    
+    var actionDate: Date = Date()
+    var countDownTimer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    @IBAction func didTapStartButton(sender: NSButton) {
+ 
+        actionDate = Date()
+        let actionDateComp = getTimerComponents(date: actionDate)
+        let settingDateComp = getTimerComponents(date: timerPicker.dateValue)
+        var finishDate = actionDate
+        
+        let hour = settingDateComp.hour!
+        let minutes = settingDateComp.minute!
+        let second = settingDateComp.second!
+        actionDate = timerPicker.dateValue
+        
+        print("\(hour)時間")
+        print("\(minutes)分")
+        print("\(second)秒")
+        
+        finishDate = finishDate.addingTimeInterval(Double(hour) * 3600.0 + Double(minutes) * 60.0 + Double(second) * 1.0)
+        print(actionDate)
+        print(finishDate)
+        
+        countDownTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.decrementSecond), userInfo: nil, repeats: true)
+        
+        timerLabel.stringValue = "\(hour):\(minutes):\(second)"
+    }
+    
+    func decrementSecond() {
+        print("decrementSecond")
+        actionDate = actionDate.addingTimeInterval(1.0 * 1)
+        print(actionDate)
+    }
+    
+    func getTimerComponents(date: Date) -> DateComponents {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour, .minute, .second], from: date)
+        
+        return components
     }
 }
 
